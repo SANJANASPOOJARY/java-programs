01edf00a5c60973cc21415e0d79927bad4c2f9b2
SANJANA <sanjanapoojary@gmail.com>
2026-03-12 23:13:19 +0530

    addition.java

---
+++ src/addition.java
@@ -0,0 +1,8 @@
+class addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 9;
+        int sum = a + b;
+        System.out.println("Sum=" + sum);
+    }
+}
