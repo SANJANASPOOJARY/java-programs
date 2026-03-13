1969922ed04239809ac3016948d14ae794433a7a
SANJANA <sanjanapoojary@gmail.com>
2026-03-13 09:16:48 +0530

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static int countWays(int n){
+        if(n==0)return 1;
+        if(n==1)return 1;
+        return countWays(n-1)+countWays(n-2);
+    }
+    public static void main(String[] args){
+        int n=3;
+        int ways=countWays(n);
+        System.out.println("Number of distinct ways:"+ways);
     }
 }
